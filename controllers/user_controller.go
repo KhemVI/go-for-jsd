d6812076a5270fb47fcd6db6561fbb3f05b31d3a
Khem <tempkhem@gmail.com>
2024-12-19 18:02:56 +0700

    feat(*): request timeout for db

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,7 +1,6 @@
 package controllers
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 	"time"
@@ -31,9 +30,6 @@ type UserController struct {
 func (uc *UserController) CreateUser(c *gin.Context) {
 	var request common.CreateUserRequest
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
 	if err := c.ShouldBindJSON(&request); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err})
 		return
@@ -46,7 +42,7 @@ func (uc *UserController) CreateUser(c *gin.Context) {
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 	}
-	_, err := uc.Collection.InsertOne(ctx, user)
+	_, err := uc.Collection.InsertOne(c.Request.Context(), user)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to create user"})
 		return
@@ -58,17 +54,14 @@ func (uc *UserController) CreateUser(c *gin.Context) {
 func (uc *UserController) GetUsers(c *gin.Context) {
 	var users []models.User
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	cursor, err := uc.Collection.Find(ctx, bson.M{})
+	cursor, err := uc.Collection.Find(c.Request.Context(), bson.M{})
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to fetch users"})
 		return
 	}
-	defer cursor.Close(ctx)
+	defer cursor.Close(c.Request.Context())
 
-	for cursor.Next(ctx) {
+	for cursor.Next(c.Request.Context()) {
 		var user models.User
 		if err := cursor.Decode(&user); err != nil {
 			fmt.Println(err)
@@ -93,10 +86,8 @@ func (uc *UserController) GetUserByID(c *gin.Context) {
 	objID, _ := primitive.ObjectIDFromHex(id)
 
 	var user models.User
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
 
-	err := uc.Collection.FindOne(ctx, bson.M{"_id": objID}).Decode(&user)
+	err := uc.Collection.FindOne(c.Request.Context(), bson.M{"_id": objID}).Decode(&user)
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
 		return
@@ -118,10 +109,7 @@ func (uc *UserController) DeleteUser(c *gin.Context) {
 	id := c.Param("id")
 	objID, _ := primitive.ObjectIDFromHex(id)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	result, err := uc.Collection.DeleteOne(ctx, bson.M{"_id": objID})
+	result, err := uc.Collection.DeleteOne(c.Request.Context(), bson.M{"_id": objID})
 	if err != nil || result.DeletedCount == 0 {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to delete user"})
 		return
